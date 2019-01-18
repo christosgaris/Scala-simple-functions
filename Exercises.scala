f585105a87c190d9a9cff6af2a056038ae323f4e
christosgaris <46817616+christosgaris@users.noreply.github.com>
2019-01-18 15:53:13 +0100

    Update Exercises.scala

--- Exercises.scala
+++ Exercises.scala
@@ -6,7 +6,7 @@ object Exercises {
   	
 
 	// 1 - Sum
-    def sum(x: Int): Int = {
+    	def sum(x: Int): Int = {
     	require(x >= 0, "Please give a number greater or equal to 0.")
     	if (x == 0) 0
         else x + sum(x - 1)
@@ -49,7 +49,7 @@ object Exercises {
         else
       		if (x > y) gcd(x - y, y)
       		else gcd(x, y - x)
-    }
+    	}
 	//Exercises.gcd(10,55)	//5
 
 
@@ -112,7 +112,7 @@ object Exercises {
 
 	val square4: (List[Int] => List[Int]) = 
 					  /*(l) => l match*/ {case Nil => Nil; case h :: t => (h * h) :: square4(t)}
-    //Exercises.squareN(List(12, 56, 32))    //List(144, 3136, 1024)
+    	//Exercises.squareN(List(12, 56, 32))    //List(144, 3136, 1024)
 
 
 	// 11 - Largest element
@@ -221,39 +221,7 @@ object Exercises {
 	//Exercises.reduce(List(23, 76, 34, 84, 24, 58), (a, b) => a.max(b)) // = 84
 
 
-	// 21 - Nth smallest number
-	def smallest(l: List[Int], nth: Int): Int = {
-
-	  	// insert(1,List(3)) = 3 :: insert(1,Nil) = 3 :: 1 :: Nil	
-	  	def insert(e: Int, sn: List[Int]): List[Int] = sn match {
-		    case Nil => e :: Nil
-		    case h :: t => if (e > h) e :: sn else h :: insert(e, t)
-	  	}
-
-	  	def loop(l2: List[Int], smallest: List[Int], size: Int): Int = l2 match {
-		    case Nil => smallest.head
-		    case h :: t => if (size < nth) loop(t, insert(h, smallest), size + 1)
-		    			else if (h < smallest.head) loop(t, insert(h, smallest.tail), size)
-		    			else loop(t, smallest, size)
-	  	}
-
-	  	loop(l, Nil, 0)
-	}
-	//val ns = List(3, 7, 1, 9)
-	//Exercises.smallest(ns, 2) // = 3 (second smallest)
-
-	/*
-	smallest(List(3, 7, 1, 9), 2)
-	= loop(List(3, 7, 1, 9), Nil, 0)
-	= loop(List(7, 1, 9), insert(3, Nil), 1)               insert -> List(3)
-	= loop(List(1, 9), insert(7, List(3)), 2)              insert -> List(7,3)
-	= loop(List(9), insert(1, List(3)), 2)                 insert -> List(3,1)
-	= loop(Nil, List(3, 1), 2)
-	= 3
-	*/
-
-
-	// 22 - Balanced
+	// 21 - Balanced
 	def balanced(a: List[Char]): Boolean = {
   		
   		def counter(a: List[Char], countL: Int, countR: Int): Boolean = {
@@ -261,14 +229,13 @@ object Exercises {
     		else if (a.head == '(') counter(a.tail, countL + 1, countR) 
     		else if(a.head == ')') counter(a.tail, countL, countR + 1) 
     		else counter(a.tail, countL, countR)
-    	}
-
-    	counter(a, 0, 0)
+    		}
+    		counter(a, 0, 0)
 	}
 	//Exercises.balanced("hello (world)".toList) // = true (balanced).
 
 
-	// 23 - Combinations of 0s and 1s
+	// 22 - Combinations of 0s and 1s
 	def combinations(n: Int): List[List[Int]] = {
 
 		def numToBin(l: List[Int], n: Int): List[Int] = {
@@ -286,7 +253,7 @@ object Exercises {
 	//Exercises.combinations(2) // = List(List(0, 0), List(0, 1), List(1, 0), List(1, 1))
 
 
-	// 24 - Count change
+	// 23 - Count change
 	def countChange(x: Int, l: List[Int]): Int = {
 		
 		def loop(a: Int, b: List[Int], c: Int): Int = {
@@ -303,7 +270,7 @@ object Exercises {
 	//Exercises.countChange(12, List(2, 3, 4)) // = 7
 
 
-	// 25 - Fibonacci list
+	// 24 - Fibonacci list
 	def fibonacciList(a: Int): List[Int] = {
 
 		def fib(n: Int): Int = {
@@ -316,93 +283,34 @@ object Exercises {
 	}
 	//Exercises.fibonacciList(10) // = List(55, 34, 21, 13, 8, 5, 3, 2, 1, 1)
 
-
-	// 26 - Quicksort
-	def quicksort(L: List[Int]): List[Int] = {
-  		if (L.length <= 1) L 
-  		else quicksort(L.filter((i: Int) => i < L.last)) ::: L.last :: quicksort(L.filter((i: Int) => i > L.last ))
-	}
-	//Exercises.quicksort(List(1, 3, 2, 5, 4)) // = List(1, 2, 3, 4, 5)
-
 	
-	// 27 - Stream of natural numbers
+	// 25 - Stream of natural numbers
 	def streamFrom(n: Int): Stream[Int] = n #:: streamFrom(n + 1)
 	//Exercises.streamFrom(11).take(8).toList // = List(11, 12, 13, 14, 15, 16, 17, 18)
 
 
-	// 28 - Moving average
-	def averages(n: Int, s: Stream[Double]): Stream[Double] = (s.take(n).sum / n) #:: averages(n, s.tail)
-	//val s = Stream(3.2, 5.3, 5.6, 4.4, 3.4, 7.5, 6.2, 9.4, 2.3)
-	//Exercises.averages(3, s).take(3).toList // = List(4.7, 5.1, 4.466666666666667)
-
-
-	// 29 - Leibniz sequence for Pi
-	def leibnizStream(): Stream[Double] = {
-		def leibnizStream2(n: Int = 0): Stream[Double] = (math.pow(-1, n) / ((2 * n) + 1)) #:: leibnizStream2(n + 1)
-		leibnizStream2()
-	}
-	//Exercises.leibnizStream().take(1000).sum * 4 // = 3.140592653839794
-
-
-	// 30 - Fibonacci stream
-	def fibonacciStream(): Stream[Int] = {
-  		def fibonacciStream2(x: Int, y: Int): Stream[Int] = (x + y) #:: fibonacciStream2(y, x + y)
-  		1 #:: 1 #:: fibonacciStream2(1, 1)
-	}
-	//Exercises.fibonacciStream.take(8).toList // = List(1, 1, 2, 3, 5, 8, 13, 21)
-
-
-	// 31 - Sieve of Eratosthenes
-	def primesStream(): Stream[Int] = {
-		def sieve(s: Stream[Int]): Stream[Int] = s.head #:: sieve(s.tail.filter(_ % s.head != 0))
-		sieve(Stream.from(2))
-	}
-	//Exercises.primesStream.take(10).toList	//List(2, 3, 5, 7, 11, 13, 17, 19, 23, 29)
-
-
-	// 32 - Minkowski distance
-	val minkowski: (Double => ((List[Double], List[Double]) => Double)) = (a) => { (M, N) => {
-
-		def **(i: Double): Double = 1/i
-
-    	def mink(a: Double, M: List[Double], N: List[Double], Total: Double = 0): Double = {
-        	if (M == Nil) Total 
-        	else mink(a: Double, M.tail, N.tail, Total + math.pow(math.abs(M.head - N.head), a))
-    	}
-
-    	math.pow(mink(a: Double, M: List[Double], N: List[Double]), **(a))
-	}}
-	//val a = List(1.0, 2.0, 3.0, 4.0, 5.0)
-	//val b = List(1.0, 2.0, 1.0, 2.0, 5.0)
-	//val euclidean = Exercises.minkowski(2)
-	//val manhattan = Exercises.minkowski(1)
-	//Exercises.minkowski(1.5)(a, b) // = 3.1748021039363987
-	//euclidean(a, b) // = 2.8284271247461903
-	//manhattan(a, b) // = 4.0
-
-
-	// 33 - Merge and reduce
+	// 26 - Merge and reduce
   	def mergeReduce(a: List[Int], b: List[Int], f1: (Int, Int) => Int, f2: (Int, Int) => Int): Int = {
 
   		/* merge function */
-    	def merge(x: List[Int], y: List[Int], f: (Int, Int) => Int): List[Int] = x match {
-    		case Nil => Nil
-    		case h :: t => if (!y.isEmpty) f(h, y.head) :: merge(t, y.tail, f) else Nil
-    	}
+  	  	def merge(x: List[Int], y: List[Int], f: (Int, Int) => Int): List[Int] = x match {
+    			case Nil => Nil
+    			case h :: t => if (!y.isEmpty) f(h, y.head) :: merge(t, y.tail, f) else Nil
+    		}
 
-    	/* reduce function */
-    	def reduce(l: List[Int], f: (Int, Int) => Int): Int = l match {
-    		case Nil => -1
-			case a :: Nil => a
-			case a :: b :: Nil => f(a, b)
-			case a :: b :: rest => reduce((f(a, b) :: rest), f)
-    	}
+	    	/* reduce function */
+    		def reduce(l: List[Int], f: (Int, Int) => Int): Int = l match {
+    			case Nil => -1
+				case a :: Nil => a
+				case a :: b :: Nil => f(a, b)
+				case a :: b :: rest => reduce((f(a, b) :: rest), f)
+    		}
 
-    	require(a != Nil, "Please give non empty lists.")
-    	require(b != Nil, "Please give non empty lists.")
+	    	require(a != Nil, "Please give non empty lists.")
+	    	require(b != Nil, "Please give non empty lists.")
 
-    	/* first we apply merge, and then we apply reduce */
-    	reduce(merge(a, b, f1), f2)
+    		/* first we apply merge, and then we apply reduce */
+    		reduce(merge(a, b, f1), f2)
   	}
   	//val a = List(3, 7, 2, 9)
 	//val b = List(1, 8, 4, 6)
@@ -411,65 +319,7 @@ object Exercises {
 	//Exercises.mergeReduce(a, b, prod, sum) // = 121
 
 
-	// 34 - Z-Score
-	def countAndMean(l: List[Double]): (Int, Double) = {
-		def loop(l: List[Double], n: Int, u: Double): (Int, Double) = l match {
-			case Nil => (n, u / n)
-			case h :: t => loop(t, n, u + h)
-		}
-		loop(l, l.length, 0.0)
-	}
-	def stdev(l: List[Double], n: Int, u: Double): Double = {
-		def loop2(l: List[Double], n: Int, u: Double, sd: Double): Double = l match {
-			case Nil => math.sqrt(sd / (n - 1))
-			case h :: t => loop2(t, n, u, sd + math.pow(h - u, 2))
-		}
-		loop2(l, l.length, u, 0.0)	
-	}
-	def zscores(l: List[Double], u: Double, sd: Double): List[(Double, Double)] = {
-		def loop3(l: List[Double], u: Double, sd: Double): List[(Double, Double)] = l match {
-			case Nil => Nil
-			case h :: t => List((h, (h - u) / sd)) ::: loop3(t, u, sd)
-		}
-		loop3(l, u, sd)
-	}
-	//val l = List(23.2, 65.5, 29.7, 65.5, 83.2, 13.9, 50.8, 1.0)
-	//val (n, u) = Exercises.countAndMean(l) // = (8, 41.6)
-	//val sd = Exercises.stdev(l, n, u) // = 28.917221759458748
-	//Exercises.zscores(l, u, sd) // = List((23.2,-0.636..), (65.5,0.826..), ...)
-
-
-	// 35 - Pearson correlation
-	def pearson(L1: List[Int], L2: List[Int]): Double = {
-
-		def calmean(L: List[Int]): Double = {
-    		def counting(L: List[Int], sum: Int = 0, counter: Int = 0): Double = {
-      			if (L == Nil) sum / counter 
-      			else counting(L.tail, sum + L.head, counter + 1)
-    		}
-    		counting(L)
-  		}
-
-  		def numerator(L1: List[Int], L2:List[Int], ans: Double = 0, L1m: Double, L2m: Double): Double = {
-    		if (L1 == Nil) ans 
-    		else numerator(L1.tail, L2.tail, ans + ((L1.head - L1m) * (L2.head - L2m)), L1m, L2m)
-  		}
-
-  		def denominator(L1: List[Int], L2: List[Int], sum1: Double = 0, sum2: Double = 0, L1m: Double, L2m: Double): Double = {
-    		if (L1 == Nil) math.sqrt(sum1) * math.sqrt(sum2) 
-    		else denominator(L1.tail, L2.tail, sum1 + math.pow(L1.head - L1m, 2), sum2 + math.pow(L2.head - L2m, 2), L1m, L2m)
-  		}
-  
-  		numerator(L1, L2, 0, calmean(L1), calmean(L2)) / denominator(L1, L2, 0, 0, calmean(L1), calmean(L2))
-	}
-	//val l1 = 1 to 10 toList
-	//val l2 = (1 to 5 toList) ++: (1 to 5 toList).reverse
-	//Exercises.pearson(l1, l1) // = 1.0
-	//Exercises.pearson(l1, l1.reverse) // = -1.0
-	//Exercises.pearson(l1, l2) // = 0.0
-
-
-	// 36 - Transpose
+	// 27 - Transpose
 	val transpose: (Matrix => Matrix) = (m: Matrix) => {
 
 		/* getHeads: get the first elements of sublists, 
@@ -500,7 +350,7 @@ object Exercises {
 	// 2 4 6
 
 
-	// 37 - Matrix multiplication
+	// 28 - Matrix multiplication
 	val multiply: ((Matrix, Matrix) => Matrix) = (a: Matrix, b: Matrix) => {
 
 		/* exactly same transpose as above, had to 
@@ -549,5 +399,4 @@ object Exercises {
 	//ab.map(_.mkString(" ")).foreach(println)
 	// 58 64
 	// 139 154
-
 }
